731787d0cdd45d12e1cc6afa89c3a1850eb1e1a6
LoreAlv <83510170+LoreAlv@users.noreply.github.com>
2025-10-11 19:30:00 +0200

    Add filtering functionality for job listings by location, contract type, experience level, and technology

--- 2_en clase/script.js
+++ 2_en clase/script.js
@@ -29,3 +29,51 @@ filter?.addEventListener("input", (event) => {
     const filtro = event.target.value.toLowerCase();
     const empleos = document.querySelectorAll(".jobCard");
 });
+
+document.getElementById("ubicacion-select").addEventListener("change", function () {
+    const ubicacion = this.value;
+    document.querySelectorAll(".jobs-listing article").forEach((article) => {
+        if (!ubicacion || article.dataset.ubicacion === ubicacion) {
+            article.style.display = "";
+        } else {
+            article.style.display = "none";
+        }
+    });
+});
+
+//filtrar por contrato
+document.getElementById("contrato-select").addEventListener("change", function () {
+    const contrato = this.value;
+    document.querySelectorAll(".jobs-listing article").forEach((article) => {
+        if (!contrato || article.dataset.contrato === contrato) {
+            article.style.display = "";
+        } else {
+            article.style.display = "none";
+        }
+    });
+});
+
+//filtrar por experiencia
+document.getElementById("experiencia-select").addEventListener("change", function () {
+    const experiencia = this.value;
+    document.querySelectorAll(".jobs-listing article").forEach((article) => {
+        if (!experiencia || article.dataset.experiencia === experiencia) {
+            article.style.display = "";
+        } else {
+            article.style.display = "none";
+        }
+    });
+});
+
+//filtrar por tecnologia
+document.getElementById("tecnologia-select").addEventListener("change", function () {
+    const tecnologia = this.value;
+    console.log(tecnologia);
+    document.querySelectorAll(".jobs-listing article").forEach((article) => {
+        if (!tecnologia || article.dataset.tecnologia.includes(tecnologia)) {
+            article.style.display = "";
+        } else {
+            article.style.display = "none";
+        }
+    });
+});
